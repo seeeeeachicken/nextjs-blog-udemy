a3bb0aa96849cc652b35784ed24ad27fb59e412a
Fujita <kento.fujita@ariseanalytics.com>
2025-02-05 15:24:53 +0900

    afo

---
+++ pages/posts/[id].js
@@ -0,0 +1,41 @@
+import { getAllPostIds, getPostData } from "../../lib/post";
+import Layout from "../components/Layout";
+import utilStyles from "../../styles/utils.module.css";
+import Head from "next/head";
+
+export async function getStaticPaths(){
+    const paths = getAllPostIds();
+    console.log("Generated Paths:", paths); // 追加
+
+    return{
+        paths,
+        fallback: false, 
+    }
+}
+
+export async function getStaticProps({params}){
+    const postData = await getPostData(params.id);
+
+    return{
+        props:{
+            postData,
+        }
+    }
+}
+
+export default function Post({postData}) {
+    return (
+        <Layout>
+            <Head>
+                <title>
+                    {postData.title}
+                </title>
+            </Head>
+            <article>
+                <h1 className={utilStyles.headingX1}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>{postData.date}</div>
+                <div dangerouslySetInnerHTML={{__html: postData.blogContentHTML}}/>
+            </article>
+        </Layout>
+    );
+}
